22ddded612c53fa82670f9affbcfb4a468eecd74
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2014-11-25 09:15:07 +0100

    better code

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import(
         "fmt"
-        "net/http"
-        // "io/ioutil"
         "github.com/daviddengcn/go-colortext"
-        "github.com/PuerkitoBio/goquery"
 )
 
 func main(){
@@ -14,30 +11,7 @@ func main(){
         fmt.Println("===================")
         ct.ChangeColor(ct.Blue, true, ct.White, false)
 
-        resp := getResponse()
-        defer resp.Body.Close()
-        //for debugging
-        // body, _ := ioutil.ReadAll(resp.Body)
-        // fmt.Println(string(body))
-
-        doc, err := goquery.NewDocumentFromResponse(resp)
-        if err != nil {
-                panic(err)
-        }
-
-        find(doc, ".summary h3 a")
-}
-
-func getResponse() *http.Response {
-        resp, err := http.Get("http://rockyj.in")
-        if err != nil {
-                panic(err)
-        }
-        return resp
-}
-
-func find(doc *goquery.Document, selector string) {
-        doc.Find(selector).Each(func(i int, s *goquery.Selection) {
-                fmt.Printf("%d: %s\n", i, s.Text())
-        })
+        scraper := NewScraper("http://rockyj.in")
+        doc := scraper.GetDocument()
+        scraper.Find(doc, ".summary h3 a")
 }
---
+++ scraper.go
@@ -0,0 +1,40 @@
+package main
+
+import(
+        "fmt"
+        "net/http"
+        "github.com/PuerkitoBio/goquery"
+)
+
+type Scraper struct {
+        url string
+}
+
+func NewScraper(url string) *Scraper {
+        s := Scraper{url}
+        return &s
+}
+
+func (s *Scraper) GetDocument() *goquery.Document {
+        resp := s.getResponse()
+        defer resp.Body.Close()
+        doc, err := goquery.NewDocumentFromResponse(resp)
+        if err != nil {
+                panic(err)
+        }
+        return doc
+}
+
+func (s *Scraper) Find(doc *goquery.Document, selector string) {
+        doc.Find(selector).Each(func(i int, s *goquery.Selection) {
+                fmt.Printf("%d: %s\n", i, s.Text())
+        })
+}
+
+func (s *Scraper) getResponse() *http.Response {
+        resp, err := http.Get(s.url)
+        if err != nil {
+                panic(err)
+        }
+        return resp
+}
