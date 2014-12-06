dff5fb3c4cfdb8cbfd5f76a17f7ad5d147394d38
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2014-12-06 21:47:18 +0100

    use gpm and gvp

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-gom install && gom build && ./scrappy
+source gvp
+gpm
+mkdir -p ./.godeps/src/github.com/rocky-jaiswal/scrappy/scraper
+cp ./scraper/scraper.go ./.godeps/src/github.com/rocky-jaiswal/scrappy/scraper/scraper.go
+go build && ./scrappy
